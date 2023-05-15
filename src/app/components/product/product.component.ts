304d674e6daae67af3040b8a094d2166d192c542
Cem Fidan <devcfidan@gmail.com>
2023-05-15 20:51:12 +0300

    17th lesson part-I

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { HttpClient } from '@angular/common/http';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
 
 @Component({
   selector: 'app-product',
@@ -6,45 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-  product1 = {
-    productId: 1,
-    productName: 'Laptop',
-    categoryId: 1,
-    unitPrice: 5000,
-  };
-  product2 = {
-    productId: 2,
-    productName: 'Desktop',
-    categoryId: 1,
-    unitPrice: 15000,
-  };
-  product3 = {
-    productId: 3,
-    productName: 'Workstation',
-    categoryId: 1,
-    unitPrice: 4500,
-  };
-  product4 = {
-    productId: 4,
-    productName: 'Server',
-    categoryId: 2,
-    unitPrice: 5500,
-  };
-  product5 = {
-    productId: 5,
-    productName: 'Tablet',
-    categoryId: 2,
-    unitPrice: 500,
-  };
+  
+  products:Product[] = [];
+  apiUrl = "https://localhost:7085/api/Products/getall";
 
-  products = [
-    this.product1,
-    this.product2,
-    this.product3,
-    this.product4,
-    this.product5,
-  ];
+  constructor(private httpClient:HttpClient) {}
 
-  constructor() {}
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.httpClient
+    .get<ProductResponseModel>(this.apiUrl)
+    .subscribe(response=>{
+      this.products = response.data
+    });
+  }
 }
