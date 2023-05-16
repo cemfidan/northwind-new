eb113efc261760b8ec65efa2bdf02a69cfbe5ed7
Cem Fidan <devcfidan@gmail.com>
2023-05-17 00:22:19 +0300

    18th lesson part-I completed

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
@@ -8,6 +9,13 @@ import { Category } from 'src/app/models/category';
 })
 export class CategoryComponent implements OnInit {
   categories: Category[] = [];
-  constructor() {}
-  ngOnInit(): void {}
+  constructor(private categoryService:CategoryService) {}
+  ngOnInit(): void {
+    this.getCategories();
+  }
+  getCategories(){
+    this.categoryService.getCategories().subscribe(response=>{
+      this.categories = response.data
+    })
+  }
 }
