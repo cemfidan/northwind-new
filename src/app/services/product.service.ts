a87e135b24b4b0caefaf652c17e3bda6e377f95c
Cem Fidan <devcfidan@gmail.com>
2023-05-16 23:48:51 +0300

    18th lesson part-I

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,17 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ProductResponseModel } from '../models/productResponseModel';
 import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
   apiUrl = 'https://localhost:7085/api/Products/getall';
-
   constructor(private httpClient: HttpClient) {}
-
-  getProducts(): Observable<ProductResponseModel> {
-    return this.httpClient.get<ProductResponseModel>(this.apiUrl);
+  getProducts(): Observable<ListResponseModel<Product>> {
+    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
   }
 }
