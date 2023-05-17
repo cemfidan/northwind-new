8fafc5e33a87a1b78c7f012194092d190fabc302
Cem Fidan <devcfidan@gmail.com>
2023-05-17 13:15:48 +0300

    18th lesson completed

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,9 +8,14 @@ import { Product } from '../models/product';
   providedIn: 'root',
 })
 export class ProductService {
-  apiUrl = 'https://localhost:7085/api/Products/getall';
+  apiUrl = 'https://localhost:7085/api/';
   constructor(private httpClient: HttpClient) {}
   getProducts(): Observable<ListResponseModel<Product>> {
-    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
+    let newPath = this.apiUrl + "products/getall"
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+  getProductsByCategory(categoryId:number): Observable<ListResponseModel<Product>> {
+    let newPath = this.apiUrl + "products/getbycategory?categoryId="+categoryId
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
   }
 }
