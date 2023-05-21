bf6d90d0ecd6ff60b0a46724c1c2e1891c1999dd
Cem Fidan <devcfidan@gmail.com>
2023-05-21 04:46:03 +0300

    a

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,7 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 export class LoginComponent implements OnInit {
   loginForm:FormGroup;
 
-  constructor (private formBuilder:FormBuilder) {}
+  constructor (private formBuilder:FormBuilder, private authService:AuthService) {}
 
   ngOnInit(): void {
     this.createLoginForm();
@@ -25,6 +26,10 @@ export class LoginComponent implements OnInit {
   login(){
     if(this.loginForm.valid){
       console.log(this.loginForm.value);
+      let loginModel = Object.assign({}, this.loginForm.value)
+      this.authService.login(loginModel).subscribe(data=>{
+        
+      })
     }
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { loginModel } from '../models/loginModel';
 import { HttpClient } from '@angular/common/http';
+import { ResponseModel } from '../models/responseModel';
+import { TokenModel } from '../models/tokenModel';
+import { SingleResponseModel } from '../models/singleResponseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +14,14 @@ export class AuthService {
   constructor(private httpClient:HttpClient) { }
 
   login(user:loginModel){
-    return this.httpClient.post(this.apiUrl+"login", loginModel);
+    return this.httpClient.post<SingleResponseModel<TokenModel>>(this.apiUrl+"login", user);
+  }
+
+  isAuthenticated(){
+    if(localStorage.getItem("token")){
+      return true;
+    } else {
+      return false;
+    }
   }
 }
