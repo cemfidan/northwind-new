144f18a25d14d4fcf90e2e2739cd6decfc2d47bc
Cem Fidan <devcfidan@gmail.com>
2023-05-22 20:20:36 +0300

    gfdgdf

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -33,7 +33,7 @@ export class LoginComponent implements OnInit {
       this.authService.login(loginModel).subscribe(response=>{
         this.toastrService.info(response.message)
         localStorage.setItem("token", response.data.token)
-      },responseError=>{
+      },responseError=>{ 
         //console.log(responseError)
         this.toastrService.error(responseError.error)
       })
